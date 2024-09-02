1e04f09206d6c7b6ffeb91c9044eb5ef565d5c08
Alberto Herrera <angelus2112@gmail.com>
2024-09-02 15:39:40 -0600

    Tarea 1

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from Koyeb"}
+
+@app.get("/tarea-1")
+def tarea_1():
+    return {"respuesta": "Primer tarea realizada"}
